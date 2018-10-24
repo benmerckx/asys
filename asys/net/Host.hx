8381a4928e5da331711c718ae14b20f5944304b4
postite <blog@postite.com>
2018-10-24 11:59:49 +0200

    reformat

--- asys/net/Host.hx
+++ asys/net/Host.hx
@@ -3,22 +3,22 @@ package asys.net;
 using tink.CoreApi;
 
 class Host {
-	public var host(default, null):String;
-	public var ip(default, null):Future<Int>;
 
-	var address:String;
+	public var host(default,null): String;
+	public var ip(default,null): Future<Int>;
+	var address: String;
 	var ipTrigger = Future.trigger();
 	#if !nodejs
 	@:allow(asys.net.Socket)
-	var instance:sys.net.Host;
+	var instance: sys.net.Host;
 	#end
 
-	public function new(name:String):Void {
+	public function new( name : String ) : Void {
 		host = name;
 		#if nodejs
-		if (~/^(\d{1,3}\.){3}\d{1,3}$/.match(name)) {
-			address = name;
-			processIp(null, address);
+		if(~/^(\d{1,3}\.){3}\d{1,3}$/.match(name)) {
+		  address = name;
+		  processIp(null, address);
 		} else {
 			js.node.Dns.lookup(name, 4, processIp);
 		}
@@ -31,41 +31,43 @@ class Host {
 	}
 
 	function processIp(err, address, ?_) {
-		if (host == address || err != null) {
+		if(host == address || err != null) {
 			ipTrigger.trigger(0);
 			return;
 		}
 
 		this.address = address;
 		var parts = address.split(".");
-		var res = 0;
+	    var res = 0;
 
-		res += Std.parseInt(parts[0]) << 24;
-		res += Std.parseInt(parts[1]) << 16;
-		res += Std.parseInt(parts[2]) << 8;
-		res += Std.parseInt(parts[3]);
+	    res += Std.parseInt(parts[0]) << 24;
+	    res += Std.parseInt(parts[1]) << 16;
+	    res += Std.parseInt(parts[2]) << 8;
+	    res += Std.parseInt(parts[3]);
 
 		ipTrigger.trigger(res);
 	}
 
-	public function toString():String {
+	public function toString(): String {
 		return address;
 	}
 
-	public function reverse():Promise<String> {
+	public function reverse(): Promise<String> {
 		#if !nodejs
 		return Future.sync(Error.catchExceptions(instance.reverse));
 		#else
 		var trigger = Future.trigger();
-		js.node.Dns.reverse(address, function(err:js.Error, res) trigger.trigger(switch err {
-			case null: Success(res.pop());
-			default: Failure(Error.withData(err.message, err));
-		}));
+		js.node.Dns.reverse(address, function(err: js.Error, res)
+			trigger.trigger(switch err {
+				case null: Success(res.pop());
+				default: Failure(Error.withData(err.message, err));
+			})
+		);
 		return trigger.asFuture();
 		#end
 	}
 
-	public static function localhost():String {
+	public static function localhost() : String {
 		#if nodejs
 		return js.node.Os.hostname();
 		#elseif (php && haxe_ver >= 4.0)
